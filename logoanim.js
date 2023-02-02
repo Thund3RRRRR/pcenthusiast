3ede9a1e13ff36658852df0d3f84e4fa8c5ed3fd
Thund3RRRRR <105231258+Thund3RRRRR@users.noreply.github.com>
2023-02-02 18:01:13 -0500

    fixed img issue

---
+++ logoanim.js
@@ -0,0 +1,31 @@
+const letters = "01";
+
+let interval = null;
+
+let target = document.querySelector("[data-value]")
+
+window.onload = event => {
+    console.log("function called")
+    let iteration = 0;
+            
+    clearInterval(interval);
+            
+    interval = setInterval(() => {
+         target.innerText = target.innerText
+        .split("")
+        .map((letter, index) => {
+            if(index < iteration) {
+                return target.dataset.value[index];
+            }
+                
+            return letters[Math.floor(Math.random() * 2)]
+            })
+        .join("");
+                
+        if(iteration >= target.dataset.value.length){ 
+            clearInterval(interval);
+        }
+                
+        iteration += 1 / 3;
+    }, 30);
+}
